c12252ae35b36aa1f405753516617f635bd1fe75
choeseonghyeon <chjcmy@naver.com>
2026-02-22 01:29:17 +0900

    revert: use OBO flow to allow user MFA as requested

--- app/domains/subscription/usecases/get_subscriptions_use_case.py
+++ app/domains/subscription/usecases/get_subscriptions_use_case.py
@@ -8,13 +8,8 @@ class GetSubscriptionsUseCase:
         self.repository = repository
 
     async def execute(self, sso_token: str) -> SubscriptionListResponse:
-        # 1. 인증 방식에 따라 토큰 획득 (Managed Identity인 경우 직방으로 서비스 토큰 사용)
-        from app.core.config import settings
-        if settings.AUTH_METHOD == "managed_identity":
-            arm_token = await security.get_service_access_token()
-        else:
-            # 기본적으로는 사용자를 대신하는 OBO 토큰 교환 시도
-            arm_token = await security.get_obo_access_token(sso_token)
+        # 1. OBO Token Exchange (사용자 권한 대행 - 휴대폰 MFA가 필요한 경우 이 경로를 사용)
+        arm_token = await security.get_obo_access_token(sso_token)
 
         # 2. Call Repository Directly
         raw_subscriptions = await self.repository.list_subscriptions(arm_token)
