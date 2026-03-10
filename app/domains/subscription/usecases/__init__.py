346f535d0343b12dcf8089712638b32633aaf5a3
Yoonsik-Shin <sys25800994@gmail.com>
2026-03-11 01:16:03 +0900

    refactor: remove separate subscription domain and move it into tenant domain

--- app/domains/subscription/usecases/__init__.py
+++
@@ -1,4 +0,0 @@
-from .get_subscription_setup_info_use_case import GetSubscriptionSetupInfoUseCase
-from .get_subscriptions_use_case import GetSubscriptionsUseCase
-
-__all__ = ["GetSubscriptionsUseCase", "GetSubscriptionSetupInfoUseCase"]
