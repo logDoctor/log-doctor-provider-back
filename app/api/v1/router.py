2e187ec42d74438dd38a834fb5a900924f67de49
Yoonsik-Shin <sys25800994@gmail.com>
2026-03-11 01:16:36 +0900

    api: update routing and endpoints for reorganized domain usecases

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -6,7 +6,6 @@ from app.domains.agent.router import router as agent_router
 from app.domains.license.router import router as license_router
 from app.domains.package.router import router as package_router
 from app.domains.report.router import router as report_router
-from app.domains.subscription.router import router as subscription_router
 from app.domains.tenant.router import router as tenant_router
 
 v1_router = APIRouter()
@@ -15,7 +14,6 @@ v1_router.include_router(template_router)
 v1_router.include_router(teams_webhook_router)
 v1_router.include_router(package_router)
 v1_router.include_router(tenant_router)
-v1_router.include_router(subscription_router)
 v1_router.include_router(agent_router)
 v1_router.include_router(report_router)
 v1_router.include_router(license_router)
